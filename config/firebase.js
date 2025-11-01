2194b47869097b05b1ab334e2896ede4314b380f
Jaimin Pansuriya <jaiminpansuriya@Jaimins-MacBook-Air.local>
2025-11-01 08:24:17 +0530

    Update backend code with latest changes

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,5 @@
 const admin = require('firebase-admin');
-const path = require('path');
-const serviceAccount = require(path.resolve(process.env.FIREBASE_SERVICE_ACCOUNT_PATH));
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
